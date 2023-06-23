0eefc2f22f7393d35d2cbb6d9e2a5b90da14cb7a
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-06-23 14:06:10 +0800

    lock on for camera

--- CameraController.gd
+++ CameraController.gd
@@ -1,9 +1,20 @@
 class_name CameraController
 extends SpringArm3D
 
+@export_category("Camera Settings")
 @export var mouse_sensitivity = 10
 @export var camera_angle = 10
 @export var camera_fov = 75
+
+@export_category("Lock On Settings")
+@export var lock_on_min_angle = 35
+@export var lock_on_max_angle = 50
+@export var lock_on_min_distance = 1
+@export var lock_on_max_distance = 10
+
+var _locked_on_enemy: Enemy = null
+var _looking_direction
+var _target_look
 
 @onready var _cam = $Camera3D
 
@@ -19,9 +30,23 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+		
+	if _locked_on_enemy:
+		_looking_direction = -global_position.direction_to(_locked_on_enemy.position)
+		_target_look = atan2(_looking_direction.x, _looking_direction.z)
+		var desired_rotation_y = lerp_angle(rotation.y, _target_look, 0.05)
 
+		var clamped_distance = clamp(position.distance_to(_locked_on_enemy.position), lock_on_min_distance, lock_on_max_distance)
+		var normalized_distance = (clamped_distance - lock_on_min_distance) / (lock_on_max_distance - lock_on_min_distance)
+		normalized_distance = smoothstep(0.0, 1.0, normalized_distance)
+		var angle = lerp(lock_on_max_angle, lock_on_min_angle, normalized_distance)
+		var desired_rotation_x = deg_to_rad(-angle)
+		
+		rotation.y = lerp(rotation.y, desired_rotation_y, 0.8)
+		rotation.x = lerp(rotation.x, desired_rotation_x, 0.05)
+		
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and not _locked_on_enemy:
 		rotation.x -= event.relative.y * mouse_sensitivity
 		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
 		
@@ -33,5 +58,4 @@ func get_lock_on_position(enemy: Enemy) -> Vector2:
 	return pos
 
 func _on_player_lock_on(enemy: Enemy):
-	if enemy:
-		look_at(enemy.position)
+	_locked_on_enemy = enemy
