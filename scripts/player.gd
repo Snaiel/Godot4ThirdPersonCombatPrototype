1e9b6fcb557c9151751b47a6f2fd2654b27acdca
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-06-29 16:42:39 +0800

    commented script

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,6 @@ extends CharacterBody3D
 @export var gravity = 20
 
 @onready var _camera_controller: CameraController = $CameraController
-@onready var _model: MeshInstance3D = $Model
 
 var _move_direction = Vector3.ZERO
 var _velocity = Vector3.ZERO
@@ -27,17 +26,24 @@ func _ready():
 	_last_physics_pos = global_position
 
 func _physics_process(delta):
-	_camera_controller.position = _camera_controller.position.slerp(position, 0.1)	
 	rotation_degrees.y = wrapf(rotation_degrees.y, -180, 180.0)
 	
+	# smoothly move the camera controller to the player's position
+	_camera_controller.position = _camera_controller.position.slerp(position, 0.1)	
+	
+	# player inputs
 	_move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	_move_direction.z = Input.get_action_strength("backward") - Input.get_action_strength("forward")
 	
 	if _lock_on_enemy:
+		# get the angle towards the lock on target and
+		# smoothyl rotate the player towards it
 		_looking_direction = -global_position.direction_to(_lock_on_enemy.global_position)
 		_target_look = atan2(_looking_direction.x, _looking_direction.z)
 		rotation.y = lerp_angle(rotation.y, _target_look, 0.2)
 		
+		# change move direction so it orbits the locked on target
+		# (not a perfect orbit, needs tuning but not unplayable)
 		if _move_direction.length() > 0.2:
 			_move_direction = _move_direction.rotated(
 				Vector3.UP, 
@@ -48,24 +54,29 @@ func _physics_process(delta):
 		_looking_direction = -Vector3(_velocity.x, 0, _velocity.z)
 		_target_look = atan2(_looking_direction.x, _looking_direction.z)
 		
+		# swivel the camera in the opposite direction so
+		# it tries to position itself back behind the player
+		# (needs playtesting idk if it's actually good behaviour)
 		if delta:
 			_camera_controller.player_moving(_move_direction, delta)
 		
+		# change move direction so it is relative to where
+		# the camera is facing
 		_move_direction = _move_direction.rotated(Vector3.UP, _camera_controller.rotation.y).normalized()		
 	
-	# Makes sure the model is rotated fully to the desired direction
+	# Makes sure the player is rotated fully to the desired direction
 	# even if pressed for a short period of time
 	if _turning:
 		if abs(rotation.y - _target_look) < 0.01:
 			_turning = false
 		rotation.y = lerp_angle(rotation.y, _target_look, 0.2)
 	
+	if Input.is_action_just_pressed("jump"):
+		_velocity.y = jump_strength
+	
 	_velocity.x = _move_direction.x * speed
 	_velocity.z = _move_direction.z * speed
 	_velocity.y -= 0 if is_on_floor() else gravity * delta
-	
-	if Input.is_action_just_pressed("jump"):
-		_velocity.y = jump_strength
 		
 	velocity = _velocity
 	move_and_slide()
