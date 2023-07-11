16fa531bb1a53fd3e4a0d63e82c1c902a440fd7d
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-07-11 18:08:03 +0800

    blend between not attacking and attacking

--- scripts/animations/attack_animations.gd
+++ scripts/animations/attack_animations.gd
@@ -9,19 +9,32 @@ signal attacking_finished
 var _level = 1
 
 # this means that there is an intent to attack
-var _intent_to_attack
+var _intent_to_attack = false
+
+# this means if an attacking animation is currently occurring
+var _attacking = false
 
 # this means that the attack animation can play
 # meant to control when the next attack plays
 # when doing successive attacks
 var _can_play_animation = false
 
-# the currently playing animation, meant to prevent
-# the attack from cancelling if the previous animation ends
-var _current_animation = ""
-	
+# will be checked to decide whether to stop
+# the attacking animatino
+var _intend_to_stop_attacking = true
 
-func _physics_process(_delta):
+
+func _ready():
+	anim_tree["parameters/Attackin/blend_amount"] = 0.0
+
+
+func _process(_delta):
+	
+	if _attacking:
+		anim_tree["parameters/Attackin/blend_amount"] = lerp(anim_tree["parameters/Attackin/blend_amount"], 1.0, 0.3)
+	else:
+		anim_tree["parameters/Attackin/blend_amount"] = lerp(anim_tree["parameters/Attackin/blend_amount"], 0.0, 0.1)		
+
 	if _can_play_animation and _intent_to_attack:
 		_can_play_animation = false
 		
@@ -29,7 +42,9 @@ func _physics_process(_delta):
 		
 		match _level:
 			1:
+				anim_tree["parameters/Attack Inward Slash/Inward Slash Trim/seek_request"] = 0.0
 				anim_tree["parameters/Attack Inward Slash/Walk Forwards Trim/seek_request"] = 0.55
+				anim_tree["parameters/Attack/transition_request"] = "attack_1"				
 			2:
 				anim_tree["parameters/Attack Outward Slash/Outwards Slash Trim/seek_request"] = 0.5
 				anim_tree["parameters/Attack Outward Slash/Walk Forwards Trim/seek_request"] = 0.55
@@ -37,15 +52,16 @@ func _physics_process(_delta):
 			3:
 				anim_tree["parameters/Attack/transition_request"] = "attack_3"
 			4:
-				anim_tree["parameters/Attack/transition_request"] = "attack_4"
 				anim_tree["parameters/Attack Quick Slash/Quick Slash Trim/seek_request"] = 0.9
-				
+				anim_tree["parameters/Attack/transition_request"] = "attack_4"
 				
 		_intent_to_attack = false		
 		
 
 func attack(level: int):
+	_attacking = true
 	_intent_to_attack = true
+	_intend_to_stop_attacking = false
 	if level == 1:
 		_can_play_animation = true
 	_level = level	
@@ -57,26 +73,22 @@ func prevent_rotation():
 	
 	
 func recieve_can_play_animation():
+#	print("PLAY")
 	_can_play_animation = true
 	
 	
 func receive_can_attack_again():
 	can_attack_again.emit(true)
+	_intend_to_stop_attacking = true
 	
 	
 func receive_cannot_attack_again():
 	can_attack_again.emit(false)
 
 
-func _on_animation_tree_animation_finished(anim_name):
-	if "combat_animations_1" in anim_name and anim_name == _current_animation:
-		anim_tree["parameters/Attacking/transition_request"] = "not_attacking"
+func receive_attack_finished():
+	if _intend_to_stop_attacking:
+		_can_play_animation = true
 		attacking_finished.emit()
-		parent_animations.movement_animations.can_rotate.emit(true)	
-		anim_tree["parameters/Attack/transition_request"] = "attack_1"
-		_current_animation = ""
-
-
-func _on_animation_tree_animation_started(anim_name):
-	if "combat_animations_1" in anim_name:
-		_current_animation = anim_name
+		parent_animations.movement_animations.can_rotate.emit(true)
+		_attacking = false
--- scripts/components/attack_component.gd
+++ scripts/components/attack_component.gd
@@ -7,7 +7,9 @@ signal can_rotate(flag: bool)
 @export var character: PlayerAnimations
 @export var attack_level = 1
 @export var can_attack = true
-@export var can_attack_again = false
+
+var _prev_attacK_level = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,33 +17,32 @@ func _ready():
 	character.attack_animations.attacking_finished.connect(_attacking_finished)
 	character.attack_animations.can_attack_again.connect(_receive_can_attack_again)
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("attack"):
 		_attack()
 
+
 func _attack():
-	if can_attack or can_attack_again:
+#	print('bruh')
+	if can_attack:
 		can_attack = false
 		attacking.emit(true)
-		
-		if can_attack_again and attack_level < 4:
-			can_attack_again = false
-			attack_level += 1
-		else:
-			attack_level = 1
-		
 		character.attack_animations.attack(attack_level)
 	
 		
 func _receive_can_attack_again(flag: bool):
-	can_attack_again = flag
-
+	can_attack = flag
+	if flag == true and  attack_level < 4:
+		attack_level += 1
+	else:
+		attack_level = 1
+	
+	
 func _attacking_finished():
 	attacking.emit(false)
-	can_attack_again = false
 	can_attack = true
-	attack_level = 1
 
 func _receive_rotation(flag: bool):
 	can_rotate.emit(flag)
