36e2561476500be80e82e6dbf6384f0ca312feb2
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-07-20 19:08:55 +0800

    free look controller

--- scripts/utilities/camera_controller.gd
+++ scripts/utilities/camera_controller.gd
@@ -3,13 +3,21 @@ extends SpringArm3D
 
 @export var player: Player
 
+@export var event_label: Label
+
 @export_category("Camera Settings")
 @export var camera_distance = 2
 @export var vertical_offset = 0.5
-@export var autonomous_speed = 3
-@export var mouse_sensitivity = 10
 @export var camera_angle = 0
 @export var camera_fov = 75
+@export var autonomous_speed = 3
+
+@export_category("Mouse Settings")
+@export var mouse_sensitivity = 10
+
+@export_category("Controller Settings")
+@export var controller_sensitivity = 10
+@export var controller_deadzone = 0.2
 
 @export_category("Lock On Settings")
 @export var lock_on_min_angle = 20.0
@@ -39,6 +47,7 @@ func _ready():
 	top_level = true
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	mouse_sensitivity = mouse_sensitivity * pow(10, -3)
+	controller_sensitivity = controller_sensitivity * pow(10, -2) / 2
 	
 	_temp_lock_on_min_angle = lock_on_min_angle
 	_temp_lock_on_max_angle = lock_on_max_angle	
@@ -72,10 +81,30 @@ func _physics_process(_delta):
 		
 		rotation.y = lerp(rotation.y, desired_rotation_y, 0.8)
 		rotation.x = lerp(rotation.x, desired_rotation_x, 0.05)
+	else:
+		var controller_look = Vector2(
+			Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
+			Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+		)	
+		
+		if controller_look.length() > controller_deadzone:
+			event_label.text = str(controller_look)
+			
+			var new_rotation_x = rotation.x - controller_look.y * controller_sensitivity
+			rotation.x = lerp(rotation.x, new_rotation_x, 0.8)
+			rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
+			
+			var new_rotation_y = rotation.y - controller_look.x * controller_sensitivity
+			rotation.y = lerp(rotation.y, new_rotation_y, 0.8)
+			rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
+			
+			event_label.text = event_label.text + "\n" + str(new_rotation_x)
+			event_label.text = event_label.text + "\n" + str(new_rotation_y)			
 		
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and not _lock_on_target:
 		_player_looking_around = true
+
 		var new_rotation_x = rotation.x - event.relative.y * mouse_sensitivity
 		rotation.x = lerp(rotation.x, new_rotation_x, 0.8)
 		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
@@ -83,6 +112,9 @@ func _unhandled_input(event):
 		var new_rotation_y = rotation.y - event.relative.x * mouse_sensitivity
 		rotation.y = lerp(rotation.y, new_rotation_y, 0.8)
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
+		
+		event_label.text = str(new_rotation_x)
+		event_label.text = event_label.text + "\n" + str(new_rotation_y)
 	else:
 		_player_looking_around = false
 		
