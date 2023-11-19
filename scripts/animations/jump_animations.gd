43c31efb292004ab98714a993a812865d72efe98
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-11-19 17:18:26 +0800

    jump and falling

--- scripts/animations/jump_animations.gd
+++ scripts/animations/jump_animations.gd
@@ -1,18 +1,62 @@
 class_name JumpAnimations
 extends BaseAnimations
 
-
 signal jumped
 
+var about_to_land: bool = false
 
+var _can_set_falling: bool = true
+var _fade_to_fall: bool = false
+
+
+func _physics_process(_delta):
+	if debug:
+		pass
+#		prints(_fade_to_fall, anim_tree["parameters/Jump and Fall Blend/blend_amount"])
+	
+	# This blends between the jump animation and the
+	# falling idle animation
+	if _fade_to_fall:
+		anim_tree["parameters/Jump and Fall Blend/blend_amount"] = lerp(
+			float(anim_tree["parameters/Jump and Fall Blend/blend_amount"]),
+			1.0,
+			0.1
+		)
+	else:
+		anim_tree["parameters/Jump and Fall Blend/blend_amount"] = lerp(
+			float(anim_tree["parameters/Jump and Fall Blend/blend_amount"]),
+			0.0,
+			0.1
+		)
+
+## Method to start the jump animation
 func start_jump() -> void:
 	anim_tree["parameters/Jump Trim/seek_request"] = 0.65
-	anim_tree["parameters/Jump/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+	anim_tree["parameters/Jump and Fall/transition_request"] = "jump_and_fall"
 
 
+## When to actually apply the jump force
 func jump_force() -> void:
 	jumped.emit()
+	_can_set_falling = true
 
 
+## The point in the jump animation where we can
+## switch to the falling idle animation
+func falling_idle() -> void:
+	if _can_set_falling and not about_to_land:
+		_can_set_falling = false
+		_fade_to_fall = true
+	
+
+## A method to signfiy when to blend from the
+## falling idle animation to the rest of the
+## jump animation to 'land'
+func jump_landing() -> void:
+	_fade_to_fall = false
+
+
+## A method call to transition from jumping
 func fade_out() -> void:
-	anim_tree["parameters/Jump/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FADE_OUT
+	anim_tree["parameters/Jump and Fall/transition_request"] = "none"
+	_fade_to_fall = false
--- scripts/components/jump_component.gd
+++ scripts/components/jump_component.gd
@@ -15,6 +15,7 @@ var jumping: bool = false
 
 var _can_emit_just_landed: bool = true
 
+@onready var _jump_raycast: RayCast3D = $JumpRaycast
 
 func _ready():
 	animations.jump_animations.jumped.connect(jump)
@@ -22,11 +23,21 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	
+	animations.jump_animations.about_to_land = _jump_raycast.is_colliding()
+	
+	# Check if we're about to land on the floor
+	if not entity.is_on_floor() and \
+		entity.velocity.y < -0.2 and \
+		_jump_raycast.is_colliding():
+
+		animations.jump_animations.jump_landing()
+	
+	# Check if we just landed on the floor
 	if entity.is_on_floor() and _can_emit_just_landed:
 		_can_emit_just_landed = false
 		jumping = false
 		movement_component.vertical_movement = false
-		animations.jump_animations.fade_out()
+		animations.jump_animations.fade_out()		
 		just_landed.emit()
 	
 	# actually_jump is true when the jump animation reaches the point
@@ -36,7 +47,7 @@ func _process(_delta: float) -> void:
 		actually_jump = false
 		movement_component.desired_velocity.y = jump_strength
 		movement_component.vertical_movement = true
-		_can_emit_just_landed = true		
+		_can_emit_just_landed = true
 
 
 func start_jump() -> void:
