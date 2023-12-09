60942a7c2eb952ed05e522fd40e9c6a974d528ec
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-12-09 17:32:53 +0800

    rng for wait id

--- scripts/beehave/wait.gd
+++ scripts/beehave/wait.gd
@@ -1,18 +1,30 @@
+@tool
+
 class_name Wait
 extends ActionLeaf
 
 @export var time: float
 @export var wait_id: int
+@export var generate_wait_id: bool
 
 var _timer: Timer = Timer.new()
 var _finished: bool = false
 var _waiting: bool = false
 
+
 func _ready():
-	_timer.timeout.connect(_timer_finished)
-	_timer.wait_time = time
-	_timer.one_shot = true
-	add_child(_timer)
+	if not Engine.is_editor_hint():
+		_timer.timeout.connect(_timer_finished)
+		_timer.wait_time = time
+		_timer.one_shot = true
+		add_child(_timer)
+
+
+func _process(_delta):
+	if Engine.is_editor_hint() and generate_wait_id:
+		generate_wait_id = false
+		var rng = RandomNumberGenerator.new()
+		wait_id = rng.randi_range(10000000, 99999999)
 
 
 ## Executes this node and returns a status code.
--- scripts/beehave/wait_block.gd
+++ scripts/beehave/wait_block.gd
@@ -1,6 +1,10 @@
+@tool
+
+class_name WaitBlock
 extends SequenceComposite
 
 @export var wait_id: int
+@export var generate_wait_id: bool
 @export var wait_time: float
 
 @export var wait_leaf: Wait
@@ -8,6 +12,14 @@ extends SequenceComposite
 
 
 func _enter_tree():
-	wait_leaf.time = wait_time
-	wait_leaf.wait_id = wait_id
-	reset_wait.wait_id = wait_id
+	if not Engine.is_editor_hint():
+		wait_leaf.time = wait_time
+		wait_leaf.wait_id = wait_id
+		reset_wait.wait_id = wait_id
+
+
+func _process(_delta):
+	if Engine.is_editor_hint() and generate_wait_id:
+		generate_wait_id = false
+		var rng = RandomNumberGenerator.new()
+		wait_id = rng.randi_range(10000000, 99999999)
