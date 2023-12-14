600e76acaaccf25cd9f63cf7464e87229033246d
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-12-14 17:06:30 +0800

    parry spam

--- scripts/components/parry_component.gd
+++ scripts/components/parry_component.gd
@@ -5,14 +5,23 @@ extends Node3D
 @export var block_component: BlockComponent
 
 var in_parry_window: bool = false
-var parry_interval: float = 0.2
 
 var _parry_timer: Timer
+var _parry_index: int = 0
+var _parry_interval: Array[float] = [
+	0.2,
+	0.1,
+	0.05,
+	0
+]
+
+var _parry_cooldown_timer: Timer
+var _parry_cooldown: float = 0.4
 
 
 func _ready() -> void:
 	_parry_timer = Timer.new()
-	_parry_timer.wait_time = parry_interval
+	_parry_timer.wait_time = _parry_interval[_parry_index]
 	_parry_timer.autostart = false
 	_parry_timer.one_shot = true
 	_parry_timer.timeout.connect(
@@ -21,16 +30,32 @@ func _ready() -> void:
 			print("parry window closed!")
 	)
 	add_child(_parry_timer)
-
-
-func _physics_process(_delta) -> void:
-	pass
+	
+	_parry_cooldown_timer = Timer.new()
+	_parry_cooldown_timer.wait_time = _parry_cooldown
+	_parry_cooldown_timer.autostart = false
+	_parry_cooldown_timer.one_shot = true
+	add_child(_parry_cooldown_timer)
 
 
 func parry() -> void:
-	if in_parry_window:
+	prints('spamming?', not _parry_cooldown_timer.is_stopped())
+	
+	if _parry_cooldown_timer.is_stopped():
+		_parry_index = 0
+	else:
+		_parry_index += 1
+	
+	if _parry_index > 3:
+		_parry_index = 3
+	
+	prints("parry interval:", _parry_interval[_parry_index])
+	
+	_parry_cooldown_timer.start()
+	if _parry_index == 3:
 		return
 	
 	in_parry_window = true
-	_parry_timer.start()
+	_parry_timer.start(_parry_interval[_parry_index])
+	
 	print("parry window open!")
