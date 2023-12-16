680228f1e0b21ad6d3b9bdcd7b2f61378fff0c5c
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-12-16 14:47:31 +0800

    reduce instability

--- scripts/components/instability_component.gd
+++ scripts/components/instability_component.gd
@@ -9,13 +9,38 @@ signal full_instability
 @export var active: bool = true
 @export var hitbox: HitboxComponent
 
-@export_category("Health")
+@export_category("Instability")
 @export var max_instability: float = 100.0
 
+@export_category("Instability Reduction")
+var reduction_pause_length: float = 2.0
+var reduction_rate: float = 5.0
+
+var _instability_reduction_pause_timer: Timer
+var _reduce_instability: bool = false
 
 var _instability: float = 0.0:
 	set(value):
 		_instability = clamp(value, 0.0, max_instability)
+
+
+func _ready():
+	_instability_reduction_pause_timer = Timer.new()
+	_instability_reduction_pause_timer.wait_time = reduction_pause_length
+	_instability_reduction_pause_timer.autostart = false
+	_instability_reduction_pause_timer.one_shot = true
+	_instability_reduction_pause_timer.timeout.connect(
+		func():
+			_reduce_instability = true
+	)
+	add_child(_instability_reduction_pause_timer)
+
+
+func _process(delta):
+	if _reduce_instability:
+		_instability -= reduction_rate * delta
+	if _instability > 0:
+		print(_reduce_instability)
 
 
 func get_instability() -> float:
@@ -25,6 +50,8 @@ func get_instability() -> float:
 func increment_instability(value: float):
 	_instability += value
 	instability_increased.emit()
+	_reduce_instability = false
+	_instability_reduction_pause_timer.start()
 
 
 func _on_hitbox_component_weapon_hit(_weapon: Sword):
