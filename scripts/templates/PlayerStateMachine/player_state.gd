d12a5d8a045acbb5c999cfca2c3540aa798e2b8e
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-12-23 12:53:42 +0800

    player state script template

---
+++ scripts/templates/PlayerStateMachine/player_state.gd
@@ -0,0 +1,23 @@
+# meta-name: Player State
+# meta-default: true
+# meta-space-indent: 4
+
+class_name PlayerState
+extends PlayerStateMachine
+
+
+func _ready():
+	super._ready()
+	
+
+
+func enter() -> void:
+	pass
+
+
+func process_player() -> void:
+	pass
+
+
+func exit() -> void:
+	pass
