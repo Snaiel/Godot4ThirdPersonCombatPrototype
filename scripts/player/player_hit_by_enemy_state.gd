e158585b284e7de185d6d7f62c4cc9263857182f
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-12-27 22:05:08 +0800

    buffer attack if hit by enemy

--- scripts/player/player_hit_by_enemy_state.gd
+++ scripts/player/player_hit_by_enemy_state.gd
@@ -2,10 +2,14 @@ class_name PlayerHitByEnemyState
 extends PlayerStateMachine
 
 
+@export var attack_state: PlayerAttackState
+
 var _incoming_weapon: Sword
 
 var _timer: Timer
 var _timer_length: float = 0.8
+
+var _pressed_attack: bool = false
 
 
 func _ready():
@@ -26,7 +30,10 @@ func _ready():
 	_timer.one_shot = true
 	_timer.timeout.connect(
 		func():
-			parent_state.transition_to_default_state()
+			if _pressed_attack:
+				parent_state.change_state(attack_state)
+			else:
+				parent_state.transition_to_default_state()
 	)
 	add_child(_timer)
 
@@ -38,10 +45,16 @@ func enter():
 	)
 	player.character.hit_and_death_animations.hit()
 	player.attack_component.interrupt_attack()
+	
+	_pressed_attack = false
+	
 	_timer.start()
 
 
 func process_player():
+	if Input.is_action_just_pressed("attack"):
+		_pressed_attack = true
+	
 	player.set_rotation_target_to_lock_on_target()
 
 
