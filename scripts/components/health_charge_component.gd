931c661571e17f84add7d8550167596831568787
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-01-08 22:23:16 +0800

    consume health charge vfx

--- scripts/components/health_charge_component.gd
+++ scripts/components/health_charge_component.gd
@@ -4,22 +4,61 @@ extends Node3D
 
 signal finished_drinking
 
-
+@export var entity: CharacterBody3D
 @export var character: CharacterAnimations
 @export var health_component: HealthComponent
 @export var recovery_amount: float = 60.0
 
+var _show_light: bool = false
+var _desired_light_energy: float
+
+@onready var particles: Array[GPUParticles3D] = [
+	$GreenSpeckles,
+	$BlueSpeckles,
+	$Chevrons
+]
+@onready var lights: Array[OmniLight3D] = [
+	$OmniLight3D,
+	$OmniLight3D2
+]
+
 
 func _ready():
+	_desired_light_energy = lights[0].light_energy
+	
+	for light in lights:
+			light.light_energy = 0.0
+	
 	character.drink_animations.gain_health.connect(
 		func():
 			health_component.health += recovery_amount
+			for particle in particles:
+				particle.restart()
+			_show_light = true
 	)
 	
 	character.drink_animations.finished.connect(
 		func():
 			finished_drinking.emit()
+			_show_light = false
 	)
+
+
+func _physics_process(_delta):
+	if _show_light:
+		for light in lights:
+			light.light_energy = lerp(
+				light.light_energy,
+				_desired_light_energy,
+				0.05
+			)
+	else:
+		for light in lights:
+			light.light_energy = lerp(
+				light.light_energy,
+				0.0,
+				0.05
+			)
 
 
 func consume_health_charge() -> void:
