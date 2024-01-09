53729d2c9ae6e0eaa56f4e697edffd919f94a31b
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-01-09 22:14:43 +0800

    smooth head rotation when not enabled

--- scripts/components/rotation/head_rotation_component.gd
+++ scripts/components/rotation/head_rotation_component.gd
@@ -2,6 +2,7 @@ class_name HeadRotationComponent
 extends Node3D
 
 
+@export var debug: bool = false
 @export var enabled: bool = true
 @export var target_visible: bool = false
 @export var skeleton: Skeleton3D
@@ -19,37 +20,41 @@ func _ready():
 	_default_target_pos = _target.position
 
 
-func _process(_delta):
-	skeleton.clear_bones_global_pose_override()
-	
+func _physics_process(_delta):
 	_target.visible = target_visible
 	
-	if not enabled:
-		return
-	
-	if desired_target_pos != Vector3.INF:
-		_target.global_position = lerp(
-			_target.global_position,
-			desired_target_pos,
-			0.02
-		)
-	else:
-		_target.position = lerp(
-			_target.position,
-			_default_target_pos,
-			0.02
-		)
+	skeleton.clear_bones_global_pose_override()
 	
 	var pose: Transform3D = skeleton.get_bone_global_pose(
 		head_idx
 	)
+	
+	if enabled:
+		if desired_target_pos != Vector3.INF:
+			_target.global_position = lerp(
+				_target.global_position,
+				desired_target_pos,
+				0.02
+			)
+		else:
+			_target.position = lerp(
+				_target.position,
+				_default_target_pos,
+				0.02
+			)
+	else:
+		_target.position = lerp(
+			_target.position,
+			Vector3.FORWARD * pose.basis.get_rotation_quaternion(),
+			0.15
+		)
 
 	pose = pose.looking_at(
 		skeleton.to_local(_target.global_position),
 		Vector3.UP,
 		true
 	)
-
+	
 	skeleton.set_bone_global_pose_override(
 		head_idx,
 		pose,
