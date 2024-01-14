f446962c2b2d4609d5b3ed72c090e52cc6b72267
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-01-14 23:24:00 +0800

    fixed improper root motion implementation

--- scripts/components/motion/root_motion_component.gd
+++ scripts/components/motion/root_motion_component.gd
@@ -6,13 +6,9 @@ extends MotionComponent
 
 
 func handle_movement(delta):
-	
-	var _desired_velocity = (
+	desired_velocity = (
 		character\
 			.anim_tree\
 			.get_root_motion_position()\
 			.rotated(Vector3.UP, entity.rotation.y + PI)
 	) / delta
-	
-	entity.velocity = _desired_velocity
-	entity.move_and_slide()
