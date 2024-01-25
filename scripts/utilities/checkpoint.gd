891753f6d14d87d6f5765187cf0b572df4731184
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-01-25 19:05:07 +0800

    checkpoint recover sfx

--- scripts/utilities/checkpoint.gd
+++ scripts/utilities/checkpoint.gd
@@ -14,6 +14,8 @@ var _previous_show_hint_value: bool = _show_hint
 @onready var respawn_point: Node3D = $RespawnPoint
 
 @onready var _recovery_particles: GPUParticles3D = $RecoveryParticles
+@onready var _recovery_audio: AudioStreamPlayer3D = $AudioRecover
+
 @onready var _player: Player = Globals.player
 @onready var _checkpoint_system: CheckpointSystem = Globals.checkpoint_system
 
@@ -49,3 +51,4 @@ func _process(_delta):
 
 func play_recovery_particles() -> void:
 	_recovery_particles.restart()
+	_recovery_audio.play()
