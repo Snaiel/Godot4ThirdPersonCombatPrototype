1a01bb267c81e31511e08326243596e39fdc24f8
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-01-25 21:17:39 +0800

    fixed background music counting

--- scripts/utilities/music_system.gd
+++ scripts/utilities/music_system.gd
@@ -13,24 +13,21 @@ var _counter: int = 0:
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
 
-#func _physics_process(_delta):
-#	print(_counter)
-
-
 func fade_to_active() -> void:
+	_counter += 1
+	
 	if not idle_music:
 		return
 	
 	idle_music = false
 	anim.play("FadeToActive")
-	_counter += 1
 
 
 func fade_to_idle() -> void:
+	_counter -= 1
+	
 	if idle_music:
 		return
-	
-	_counter -= 1
 	
 	if _counter != 0:
 		return
