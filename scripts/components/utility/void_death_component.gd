5f97ac047ffdc60439a2eea9d4a854ee7b636613
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-06 21:44:12 +0800

    fixed void death component

--- scripts/components/utility/void_death_component.gd
+++ scripts/components/utility/void_death_component.gd
@@ -9,8 +9,7 @@ extends Node
 func _ready() -> void:
 	Globals.void_death_system.fallen_into_the_void.connect(
 		func(body: Node3D):
-			if body != self:
-				return
+			if body != entity: return
 			health_component.deal_max_damage = true
 			health_component.decrement_health(1)
 			var timer: SceneTreeTimer = get_tree().create_timer(5)
