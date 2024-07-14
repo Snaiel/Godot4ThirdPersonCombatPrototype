1cd6768814a2ef9d8b9112da38ffad4275f97dae
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-14 15:15:13 +0800

    slight vertical oscillation to dizzy stars

--- scripts/utilities/dizzy_stars.gd
+++ scripts/utilities/dizzy_stars.gd
@@ -4,10 +4,19 @@ extends Node3D
 
 @export var speed: float = 360
 
+@onready var stars: Node3D = $Stars
+
 
 func _process(delta: float) -> void:
-	$Stars.rotation_degrees.y = wrapf(
-		$Stars.rotation_degrees.y + speed * delta,
+	var r: float = stars.rotation_degrees.y
+	stars.rotation_degrees.y = wrapf(
+		r + speed * delta,
 		0.0,
 		360
 	)
+	
+	var count: int = stars.get_child_count()
+	for i in count:
+		stars.get_child(i).position.y = 0.1 * sin(
+			2 * deg_to_rad(r + (i * (360 / float(count))))
+		)
