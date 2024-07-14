9ee24e06bdbec94e66843e58a9920d3c0b3144ec
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-14 13:45:23 +0800

    player rotation script lightly more readable

--- scripts/components/rotation/player_rotation_component.gd
+++ scripts/components/rotation/player_rotation_component.gd
@@ -14,14 +14,16 @@ func _ready() -> void:
 	player = entity as Player
 	target_look = _camera_controller.rotation.y
 	
-	looking_direction = looking_direction.rotated(Vector3.UP, _camera_controller.rotation.y).normalized()
+	looking_direction = looking_direction.rotated(
+		Vector3.UP,
+		_camera_controller.rotation.y
+	).normalized()
 
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
 	
-	if not can_rotate:
-		return
+	if not can_rotate: return
 	
 	var _input_direction: Vector3 = player.input_direction
 	var _last_input_on_ground: Vector3 = player.last_input_on_ground
@@ -35,7 +37,9 @@ func _physics_process(delta: float) -> void:
 		
 		# get the angle towards the lock on target and
 		# smoothly rotate the player towards it
-		looking_direction = player.global_position.direction_to(target.global_position)
+		looking_direction = player.global_position.direction_to(
+			target.global_position
+		)
 		target_look = atan2(-looking_direction.x, -looking_direction.z)
 		
 		# This makes the rotation smoother when the player is locked
@@ -48,14 +52,15 @@ func _physics_process(delta: float) -> void:
 		else:
 			rotation_weight = 0.2
 		
-		player.rotation.y = lerp_angle(player.rotation.y, target_look, rotation_weight)
+		player.rotation.y = lerp_angle(
+			player.rotation.y, target_look, rotation_weight
+		)
 		
 		# change move direction so it orbits the locked on target
 		# (not a perfect orbit, needs tuning but not unplayable)
 		if move_direction.length() > 0.2:
 			move_direction = move_direction.rotated(
-				Vector3.UP,
-				target_look + sign(move_direction.x) * 0.13
+				Vector3.UP, target_look + sign(move_direction.x) * 0.13
 			).normalized()
 		
 	elif _input_direction.length() > 0.2:
@@ -69,18 +74,20 @@ func _physics_process(delta: float) -> void:
 		elif can_rotate:
 			# if player can't move but they can rotate,
 			# rotate the player based on input instead
-			looking_direction = Vector3(_input_direction.x, 0, _input_direction.z)
-			looking_direction = looking_direction.rotated(Vector3.UP, _camera_controller.rotation.y).normalized()
+			looking_direction = Vector3(
+				_input_direction.x, 0, _input_direction.z
+			)
+			looking_direction = looking_direction.rotated(
+				Vector3.UP, _camera_controller.rotation.y
+			).normalized()
 		
 		# retrieve desired angle from looking direction
 		target_look = atan2(-looking_direction.x, -looking_direction.z)
-
-
+		
 		# swivel the camera in the opposite direction so
 		# it tries to position itself back behind the player
 		# (needs playtesting idk if it's actually good behaviour)
 		if delta and not target:
-			
 			if player.is_on_floor():
 				_camera_controller.player_moving(
 					move_direction,
@@ -88,24 +95,31 @@ func _physics_process(delta: float) -> void:
 					delta
 				)
 			else:
-				
 				# artifically keeps the input from the ground
 				# whilst mid air to simulate keeping momentum
 				if abs(_last_input_on_ground.z) > 0.2:
 					move_direction.z = sign(_last_input_on_ground.z)
 					
-				if abs(_last_input_on_ground.x) > 0.2 and abs(_last_input_on_ground.z) < 0.2:
-					# if moving left or right relative to camera before jumping,
-					# slow down opposite sidewards movement mid air
-					if is_equal_approx(_input_direction.x, -_last_input_on_ground.x):
-						move_direction.x = _input_direction.normalized().x * 0.2
+				if abs(_last_input_on_ground.x) > 0.2 and \
+				abs(_last_input_on_ground.z) < 0.2:
+					# if moving left or right relative to camera before
+					# jumping, slow down opposite sidewards movement mid air
+					if is_equal_approx(
+						_input_direction.x, -_last_input_on_ground.x
+					):
+						move_direction.x = \
+							_input_direction.normalized().x * 0.2
 				else:
 					# reduce side to side movement mid air
 					move_direction.x = move_direction.x * 0.6
-					# if moving forwards or backwards relative to camera before jumping,
-					# slow down opposite movement mid air
-					if is_equal_approx(_input_direction.z, -_last_input_on_ground.z) and abs(_input_direction.z) > 0.2:
-						move_direction.z = _input_direction.normalized().z * 0.2
+					# if moving forwards or backwards relative to camera
+					# before jumping, slow down opposite movement mid air
+					if is_equal_approx(
+						_input_direction.z, -_last_input_on_ground.z
+					) and \
+					abs(_input_direction.z) > 0.2:
+						move_direction.z = \
+							_input_direction.normalized().z * 0.2
 				
 				# if coming from a jump standing still
 				if _last_input_on_ground.length() < 0.1:
@@ -114,9 +128,11 @@ func _physics_process(delta: float) -> void:
 		
 		# change move direction so it is relative to where
 		# the camera is facing
-		move_direction = move_direction.rotated(Vector3.UP, _camera_controller.rotation.y)
-
-
+		move_direction = move_direction.rotated(
+			Vector3.UP, _camera_controller.rotation.y
+		)
+	
+	
 	# the massive not condition ensures that the player does not rotate
 	# the opposite way if mid air and the player is inputting to go in
 	# the opposite direction from their original momentum on the ground
@@ -125,13 +141,15 @@ func _physics_process(delta: float) -> void:
 			not player.is_on_floor() and \
 			is_equal_approx(_input_direction.z, -_last_input_on_ground.z) and \
 			abs(_input_direction.z) > 0.2
-			) and \
+		) and \
 		not (
 			not player.is_on_floor() and \
 			is_equal_approx(_input_direction.x, -_last_input_on_ground.x) and \
 			abs(_input_direction.x) > 0.2
-			) and \
-		not (not player.is_on_floor() and _last_input_on_ground.length() < 0.1):
+		) and \
+		not (
+			not player.is_on_floor() and _last_input_on_ground.length() < 0.1
+		):
 		
 		# Makes sure the player is rotated fully to the desired direction
 		# even if pressed for a short period of time
@@ -143,7 +161,7 @@ func _physics_process(delta: float) -> void:
 	# rotate slowly during input. don't turn all the way
 	if _freelook_turn and not _turn_all_the_way:
 		_freelook_turn = false
-		player.rotation.y = lerp_angle(player.rotation.y, target_look, 0.03)		
+		player.rotation.y = lerp_angle(player.rotation.y, target_look, 0.03)
 
 
 func get_rotation_difference(t: Node3D) -> float:
