8f8ab7624eae6ee43ca9a02248d84a7c03874894
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-20 17:07:51 +0800

    player thrust speed

--- scripts/animations/subclasses/melee/attacks/thrust.gd
+++ scripts/animations/subclasses/melee/attacks/thrust.gd
@@ -7,6 +7,8 @@ func _ready():
 
 
 func play_attack():
+	anim_tree.set(&"parameters/Attack Forward Thrust/Forward Thrust Trim/seek_request", 0.0)
+	anim_tree.set(&"parameters/Attack Forward Thrust/Forward Thrust Speed/scale", 1.5)
 	anim_tree.set(&"parameters/Melee Attack/transition_request", &"thrust")
 
 
