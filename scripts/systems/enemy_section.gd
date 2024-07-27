07dd874146a0ffb3f813ec24de0a1c7f25af7356
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-27 17:01:04 +0800

    spread out enemy section adding and removing

--- scripts/systems/enemy_section.gd
+++ scripts/systems/enemy_section.gd
@@ -2,32 +2,55 @@ class_name EnemySection
 extends Area3D
 
 
+enum NodeOperation {ADD, REMOVE, NONE}
+
 @onready var enemies: Node3D = $Enemies
 @onready var patrols: Node3D
+
+var enemy_nodes: Array[Node]
+var current_operation: NodeOperation = NodeOperation.NONE
+
+var _time_gap: float = 0.2
+var _timer: float
 
 
 func _ready():
 	patrols = get_node_or_null("Patrols")
 	
-	remove_child(enemies)
+	enemy_nodes = enemies.get_children()
+	for child in enemy_nodes:
+		enemies.remove_child(child)
 	_set_patrols_enabled(false)
 	
-	body_entered.connect(
-		func(_body: Node3D):
-			print("Player entered enemy section: " + name)
-			add_child(enemies)
+	body_entered.connect(func(_body: Node3D): _add_enemies())
+	body_exited.connect(func(_body: Node3D): _remove_enemies())
+	
+	_timer = _time_gap
+
+
+func _process(delta: float) -> void:
+	if current_operation == NodeOperation.NONE: return
+	
+	_timer -= delta
+	
+	if _timer > 0: return
+	
+	if current_operation == NodeOperation.ADD:
+		if len(enemy_nodes) > 0:
+			enemies.add_child(enemy_nodes.pop_front())
+		else:
+			current_operation = NodeOperation.NONE
 			Globals.checkpoint_system.set_node_owner_to_enemies(
 				enemies, enemies
-			)
-			_set_patrols_enabled(true)
-	)
+				)
+	elif current_operation == NodeOperation.REMOVE:
+		if enemies.get_child_count() > 0:
+			var child = enemies.get_child(0)
+			enemies.remove_child(child)
+		else:
+			current_operation = NodeOperation.NONE
 	
-	body_exited.connect(
-		func(_body: Node3D):
-			print("Player left enemy section: " + name)
-			remove_child(enemies)
-			_set_patrols_enabled(false)
-	)
+	_timer = _time_gap
 
 
 func _notification(what: int) -> void:
@@ -36,6 +59,21 @@ func _notification(what: int) -> void:
 		for node in enemies.get_children():
 			var enemy: Enemy = node
 			BeehaveDebuggerMessages.unregister_tree(enemy.beehave_tree.get_instance_id())
+
+
+func _add_enemies() -> void:
+	print("Player entered enemy section: " + name)
+	current_operation = NodeOperation.ADD
+	_timer = _time_gap
+	_set_patrols_enabled(true)
+
+
+func _remove_enemies() -> void:
+	print("Player left enemy section: " + name)
+	current_operation = NodeOperation.REMOVE
+	_timer = _time_gap
+	enemy_nodes = enemies.get_children()
+	_set_patrols_enabled(false)
 
 
 func _set_patrols_enabled(enabled: bool) -> void:
