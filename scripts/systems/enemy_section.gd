03f369c4109c836e8ce0cfe28310989d9af33a3b
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-07-27 14:04:12 +0800

    enemy section manage patrols process mode

--- scripts/systems/enemy_section.gd
+++ scripts/systems/enemy_section.gd
@@ -3,11 +3,14 @@ extends Area3D
 
 
 @onready var enemies: Node3D = $Enemies
+@onready var patrols: Node3D
 
 
 func _ready():
+	patrols = get_node_or_null("Patrols")
 	
 	remove_child(enemies)
+	_set_patrols_enabled(false)
 	
 	body_entered.connect(
 		func(_body: Node3D):
@@ -16,18 +19,28 @@ func _ready():
 			Globals.checkpoint_system.set_node_owner_to_enemies(
 				enemies, enemies
 			)
+			_set_patrols_enabled(true)
 	)
 	
 	body_exited.connect(
 		func(_body: Node3D):
 			print("Player left enemy section: " + name)
 			remove_child(enemies)
+			_set_patrols_enabled(false)
 	)
 
 
-func _notification(what):
+func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE:
 		print("DELETE")
 		for node in enemies.get_children():
 			var enemy: Enemy = node
 			BeehaveDebuggerMessages.unregister_tree(enemy.beehave_tree.get_instance_id())
+
+
+func _set_patrols_enabled(enabled: bool) -> void:
+	if not patrols: return
+	if enabled:
+		patrols.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		patrols.process_mode = Node.PROCESS_MODE_DISABLED
